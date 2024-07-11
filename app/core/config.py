8827d0654ff7ea0c0ac103ea1a17c5f051aedadb
AlexMuzzy <alexmuzzy33@gmail.com>
2024-07-11 23:19:39 +0100

    move to .env file

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Example FastAPI Project"
-    PROJECT_VERSION: str = "0.1.0 (dev)"
-    DEBUG: bool = False
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+    DEBUG: bool
 
     class Config:
         env_file = ".env"
