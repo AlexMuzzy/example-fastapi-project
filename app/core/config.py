ce0aa52fa14709a989d56d3df2f3db0e63c2bd0c
AlexMuzzy <alexmuzzy33@gmail.com>
2024-07-17 22:32:49 +0100

    remove env file

--- app/core/config.py
+++ app/core/config.py
@@ -3,13 +3,13 @@ from os import environ
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str
-    PROJECT_VERSION: str
-    DEBUG: bool
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_HOST: str
-    DB_NAME: str
+    PROJECT_NAME: str = "Example FastAPI Project"
+    PROJECT_VERSION: str = "0.1.0 (dev)"
+    DEBUG: bool = False
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "#KQrwQi8az4j.D9"
+    DB_HOST: str = "db.nbkaacxetceifqkzbgkf.supabase.co:5432"
+    DB_NAME: str = "postgres"
 
     class Config:
         env_file = ".env.production" if environ.get("ENV") == "production" else ".env"
