60543326ada6d05661c5f6dbed3ebd58607756cb
AlexMuzzy <alexmuzzy33@gmail.com>
2024-07-19 00:14:45 +0100

    switch to using .env file

--- app/core/config.py
+++ app/core/config.py
@@ -6,13 +6,13 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Example FastAPI Project"
     PROJECT_VERSION: str = "0.1.0 (dev)"
     DEBUG: bool = False
-    DB_USER: str = "postgres"
-    DB_PASSWORD: str = "#KQrwQi8az4j.D9"
-    DB_HOST: str = "db.nbkaacxetceifqkzbgkf.supabase.co:5432"
-    DB_NAME: str = "postgres"
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
 
     class Config:
-        env_file = ".env.production" if environ.get("ENV") == "production" else ".env"
+        env_file = ".env"
 
 
 settings = Settings()
